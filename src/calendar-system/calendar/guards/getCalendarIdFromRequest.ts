75e34b89608989d77740728b812633d491cce8d5
Miconbeko <stepanov.m2004@gmail.com>
2024-03-23 16:39:06 +0200

    fix: get calendar id from field calendar

--- src/calendar-system/calendar/guards/getCalendarIdFromRequest.ts
+++ src/calendar-system/calendar/guards/getCalendarIdFromRequest.ts
@@ -4,5 +4,7 @@ import { FullCalendarDto } from '../dto/full-calendar.dto';
 export const getCalendarIdFromRequest = async (
   req: RequestType,
 ): Promise<FullCalendarDto[`_id`]> => {
-  return req.body._id || req.body.event || req.query.calendarId;
+  return (
+    req.body._id || req.body.event || req.body.calendar || req.query.calendarId // TODO: wtf is req.body.event. + refactor for getting id from calendar object in body
+  );
 };
